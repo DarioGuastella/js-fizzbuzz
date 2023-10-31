0ac19a3ebd4c0dfdbb8ad17f775a1ca0140ecfea
DarioGuastella <jimmyguastella@gmail.com>
2023-10-31 10:51:38 +0100

    snellisce il codice

--- js/main.js
+++ js/main.js
@@ -1,40 +1,25 @@
 const container = document.getElementById("col");
 for (let i = 1; i <= 100; i++) {
+    const newDiv = document.createElement("div");
+    const newParagraph = document.createElement("p");
+    newDiv.classList.add("box");
+    container.append(newDiv);
+    newDiv.append(newParagraph);
     if (i % 15 == 0) {
-        const newDiv = document.createElement("div");
-        const newParagraph = document.createElement("p");
         newParagraph.innerHTML = "FizzBuzz" ;
-        newDiv.classList.add("box");
         newDiv.classList.add("bgRed");
-        container.append(newDiv);
-        newDiv.append(newParagraph);
         console.log("FizzBuzz");
     } else if (i % 5 == 0) {
-        const newDiv = document.createElement("div");
-        const newParagraph = document.createElement("p");
         newParagraph.innerHTML = "Buzz";
-        newDiv.classList.add("box");
         newDiv.classList.add("bgOrange");
-        container.append(newDiv);
-        newDiv.append(newParagraph);
         console.log("Buzz");
     } else if (i % 3 == 0) {
-        const newDiv = document.createElement("div");
-        const newParagraph = document.createElement("p");
         newParagraph.innerHTML = "Fizz" ;
-        newDiv.classList.add("box");
         newDiv.classList.add("bgGreen");
-        container.append(newDiv);
-        newDiv.append(newParagraph);
         console.log("Fizz");
     } else {
-        const newDiv = document.createElement("div");
-        const newParagraph = document.createElement("p");
         newParagraph.innerHTML = i ;
-        newDiv.classList.add("box");
         newDiv.classList.add("bgBlue");
-        container.append(newDiv);
-        newDiv.append(newParagraph);
         console.log(i);
     }
 }
