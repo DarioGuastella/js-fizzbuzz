f8cb8d0d4a1463326755ed1749233ffaa61758f9
DarioGuastella <jimmyguastella@gmail.com>
2023-10-31 11:19:52 +0100

    aggiunge classi in unica stringa

--- js/main.js
+++ js/main.js
@@ -2,22 +2,21 @@ const container = document.getElementById("col");
 for (let i = 1; i <= 100; i++) {
     const newDiv = document.createElement("div");
     const newParagraph = document.createElement("p");
-    newDiv.classList.add("box");
     container.append(newDiv);
     newDiv.append(newParagraph);
     if (i % 15 == 0) {
         newParagraph.innerHTML = "FizzBuzz" ;
-        newDiv.classList.add("bgRed");
+        newDiv.classList.add("bgRed", "box");
     } else if (i % 5 == 0) {
         newParagraph.innerHTML = "Buzz";
-        newDiv.classList.add("bgOrange");
+        newDiv.classList.add("bgOrange", "box");
     } else if (i % 3 == 0) {
         newParagraph.innerHTML = "Fizz" ;
-        newDiv.classList.add("bgGreen");
+        newDiv.classList.add("bgGreen", "box");
         console.log("Fizz");
     } else {
         newParagraph.innerHTML = i ;
-        newDiv.classList.add("bgBlue");
+        newDiv.classList.add("bgBlue", "box");
     }
 }
 console.log("Conteggio completato");
